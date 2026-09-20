fedf8baa3db3ede32b4aab3abfb6a766a3920682
mohammedabdallashamakh <3.30583785e+08+mohammedabdallashamakh@users.noreply.github.com>
2026-09-20 12:54:58 +0000

    Render landing page on GitHub Pages path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,12 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
 
 function Router() {
-  return (
-    <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/lahtha-website/"} component={Home} />
-      <Route path={"/404"} component={NotFound} />
-      {/* Final fallback route */}
-      <Route component={NotFound} />
-    </Switch>
-  );
+  return <Home />;
 }
 
 // NOTE: About Theme
